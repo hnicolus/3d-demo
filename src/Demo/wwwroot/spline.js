142598ece66b523997e601e0d5f27855ca840457
Nicolas <nicolusmaluleke@gmail.com>
2021-07-20 03:32:35 +0200

    updated

--- src/Demo/wwwroot/spline.js
+++ src/Demo/wwwroot/spline.js
@@ -2,7 +2,5 @@
 const app = new SpeRuntime.Application();
 const elementq = document.getElementById("canvas3d")
 function runSplineScene(element,sceneObject) {
-	console.log(element);
-	console.log("element q :",elementq)
-	app.start(sceneObject,element);
+	app.start(sceneObject, elementq);
 }
